653c7e1343b9b03e0e13276a52fec851e5a3ec40
Ivan Sungurov <isungurov@issart.com>
2010-04-19 12:43:44 +0700

    fixed wrong empty type

--- reviewboard/notifications/email.py
+++ reviewboard/notifications/email.py
@@ -237,7 +237,7 @@ def mail_review_request(user, review_request, changedesc=None):
         reply_message_id = review_request.email_message_id
         extra_recipients = harvest_people_from_review_request(review_request)
     else:
-        extra_recipients = None
+        extra_recipients = []
 
     for group in review_request.target_groups.all():
         for address in get_email_addresses_for_group(group):
