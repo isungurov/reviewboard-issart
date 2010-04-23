6dedb06feb84b9b4dbfc332279288d05963d039d
Ivan Sungurov <isungurov@issart.com>
2010-04-23 15:45:06 +0700

    fixed determining class instance type

--- reviewboard/notifications/email.py
+++ reviewboard/notifications/email.py
@@ -138,7 +138,7 @@ def send_review_mail(user, review_request, subject, in_reply_to,
 
     if extra_recipients:
         for recipient in extra_recipients:
-            if type(recipient) is basestring:
+            if isinstance(recipient, basestring):
                 recipients.add(recipient)
             elif recipient.is_active:
                 recipients.add(get_email_address_for_user(recipient))
